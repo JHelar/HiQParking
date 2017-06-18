8725de5da59b1462e02281b393b4fed493d2181a
John Larsson <John Larsson>
2017-06-18 12:53:02 +0200

    Firebsase test working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbpfvIZP4ub4Ua7ceZGhYVlNwSr6UpdFI",
+  authDomain: "hiqparkingv3.firebaseapp.com",
+  databaseURL: "https://hiqparkingv3.firebaseio.com",
+  projectId: "hiqparkingv3",
+  storageBucket: "hiqparkingv3.appspot.com",
+  messagingSenderId: "877601951339"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +24,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
